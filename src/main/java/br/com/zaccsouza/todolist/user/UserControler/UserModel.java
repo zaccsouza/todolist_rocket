ca1b32722eba64b7dcbb29255e99a642c3b68d08
Zaqueu Souza <zaqueu.souza.jr@gmail.com>
2023-10-16 01:29:19 -0300

    updated:   as classes UserControlller e UserModel

--- src/main/java/br/com/zaccsouza/todolist/user/UserControler/UserModel.java
+++ src/main/java/br/com/zaccsouza/todolist/user/UserControler/UserModel.java
@@ -1,5 +1,11 @@
 package br.com.zaccsouza.todolist.user.UserControler;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     
+    private String name;
+    private String username;
+    private String password;
 }
